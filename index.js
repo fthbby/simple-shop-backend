5a2c50450fe578bfbb6571480d1b414d5bf32cfc
Nat <fthbby@gmail.com>
2023-12-28 22:39:10 -0800

    added prodcut schema

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.json()); 
-
+app.use(bodyParser.json());
+const mongoose = require("mongoose");
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`server started on port: ${process.env.PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connected successful!");
+  })
+  .catch((err) => {
+    console.log("DB not connected", err.message);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
---
+++ src/models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const statusTypes = ["active", "draft"];
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+    unique: false,
+  },
+  price: {
+    type: Number,
+    // required: true,
+  },
+  category: {
+    type: String,
+    default: "",
+  },
+  description: {
+    type: String,
+    // required:true,
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  status: {
+    type: String,
+    default: "draft",
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
