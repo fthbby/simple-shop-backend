3159d7ca905cf5726509d9c887534308d7484659
Nat <fthbby@gmail.com>
2023-12-28 19:13:37 -0800

    basic server structure setup

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const express = require('express')
-const app = express()
 
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json()); 
+
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server started on port: ${process.env.PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
